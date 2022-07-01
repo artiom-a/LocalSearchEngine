871994f8ba9fd111fcdadf388f3394c4de984cbf
AbramyanAA <abramyanabramyan@yandex.ru>
2022-07-01 16:35:48 +0300

    Russian lemmas counter finished

--- src/main/java/club/dagomys/lemmatisator/scr/LemmaCounter.java
+++ src/main/java/club/dagomys/lemmatisator/scr/LemmaCounter.java
@@ -17,7 +17,7 @@ import java.util.stream.Collectors;
 public class LemmaCounter {
     private final LuceneMorphology russianMorphology;
     private final String text;
-    private final Map<String, Integer> wordsMap;
+    private Map<String, Long> wordsMap;
 
     public LemmaCounter(String text) throws IOException {
         this.text = text;
@@ -26,58 +26,28 @@ public class LemmaCounter {
         countLemmas();
     }
 
-    private Map<String, Integer> countLemmas() {
+    private Map<String, Long> countLemmas() {
         String[] replacedtext = text.toLowerCase(Locale.ROOT).replaceAll("—", "")
                 .replaceAll("\\p{Punct}|[0-9]", "").split("\\s+");
-
-        List<String> morphList = Arrays.stream(replacedtext).map(russianMorphology::getMorphInfo).filter(morph -> !isAuxiliaryPartsOfSpeech(morph.get(0))).flatMap(List::stream).collect(Collectors.toList());
-        Map<String, Long> normList = Arrays.stream(replacedtext).map(russianMorphology::getNormalForms).collect(Collectors.groupingBy(normalWord->normalWord.get(0), Collectors.counting()));
-
-
-
-
-        morphList.forEach(System.out::println);
-        normList.entrySet().forEach(System.out::println);
-//        normList.forEach(System.out::println);
-        List<String> necessaryWords = new ArrayList<>();
-//        for (String morph : morphList) {
-//            if (!isAuxiliaryPartsOfSpeech(morph)) {
-//                necessaryWords.add(morph);
-//            }
-//
-//        }
-
-//        for (String s : replacedtext){
-//            System.out.println(russianMorphology.getMorphInfo(s).size());
-//            russianMorphology.getMorphInfo(s).forEach(System.out::println);
-//        }
-        List<String> normalFormList;
-        for (String morph : necessaryWords) {
-            normalFormList = russianMorphology.getNormalForms(morph);
-            for (String word : normalFormList) {
-                wordsMap.merge(word, 1, Integer::sum);
-            }
-
-        }
-//        wordsMap.keySet().stream().map(russianMorphology::getMorphInfo).collect(Collectors.toSet()).forEach(System.out::println);
-
-//        wordsMap.keySet().forEach(russianMorphology::getNormalForms);
-//        wordsMap.entrySet().forEach(System.out::println);
-
+        List<String> morphList = Arrays.stream(replacedtext).filter(morph -> !isAuxiliaryPartsOfSpeech(morph)).collect(Collectors.toList());
+        wordsMap = morphList.stream().map(russianMorphology::getNormalForms).collect(Collectors.groupingBy(normalWord -> normalWord.get(0), Collectors.counting()));
         return wordsMap;
     }
 
 
-    public Map<String, Integer> getWordsMap() {
+    public Map<String, Long> getWordsMap() {
         return wordsMap;
     }
 
     private boolean isAuxiliaryPartsOfSpeech(String word) {
-        return word.contains("СОЮЗ") |
-                word.contains("МЕЖД") |
-                word.contains("МС") |
-//                word.contains("МС-П") |
-                word.contains("ПРЕДЛ") |
-                word.contains("КР_ПРИЛ");
+        List<String> morphs = russianMorphology.getMorphInfo(word);
+        for (String morph : morphs) {
+            return morph.contains("СОЮЗ") |
+                    morph.contains("МЕЖД") |
+                    morph.contains("МС") |
+                    morph.contains("ПРЕДЛ") |
+                    morph.contains("КР_ПРИЛ");
+        }
+        return false;
     }
 }
