223879e5d2b2f5dfd955571a98029c376976104d
ArtemA <abramyanabramyan@yandex.ru>
2024-05-09 13:58:36 +0300

    Merge remote-tracking branch 'origin/master'

--- src/main/java/club/dagomys/siteparser/src/MainSiteParser.java
+++ src/main/java/club/dagomys/siteparser/src/MainSiteParser.java
@@ -3,24 +3,19 @@ package club.dagomys.siteparser.src;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import java.io.IOException;
 
 @SpringBootApplication
 @OpenAPIDefinition
-public class MainSiteParser extends SpringBootServletInitializer {
+public class MainSiteParser{
     public static void main(String[] args) throws IOException {
         SpringApplication app = new SpringApplication(MainSiteParser.class);
         app.setBannerMode(Banner.Mode.OFF);
         app.setLazyInitialization(true);
+        app.setWebApplicationType(WebApplicationType.NONE);
         app.run(args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(MainSiteParser.class);
     }
 }
